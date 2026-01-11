057cd0d05499bf462c6baf26fb62ce3b79e8f591
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-12 00:45:13 +0200

    cool phone book project

---
+++ PhoneBookSystem/src/phonebooksystem/PhoneBookSystem.java
@@ -0,0 +1,19 @@
+
+package phonebooksystem;
+
+
+
+  public class PhoneBookSystem {
+     
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainMenu menu = new mainMenu();
+        menu.setVisible(true);
+    }
+    
+}
